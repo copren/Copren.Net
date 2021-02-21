9bbbefca95894b494dc9b6c8f434521467ff0078
Austin Hanson <austin.hanson@centralsquare.com>
2021-02-20 23:43:48 -0600

    Removing asteroid sample

--- Samples/Asteroid/build.sh
+++
@@ -1,3 +0,0 @@
-dotnet build --framework netcoreapp2.0 Asteroid.sln
-
-FrameworkPathOverride="/Library/Frameworks/Mono.framework/Versions/5.4.1/lib/mono/4.6.1-api/" dotnet build --framework net461 Asteroid.Shared/Asteroid.Shared.csproj
